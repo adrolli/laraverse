c252eb637597502a3bef2a6fb1f727c0c17c7a56
Alf Drollinger <alf@drollinger.info>
2023-09-14 22:22:21 +0200

    Update welcome.blade.php

--- resources/views/livewire/frontend/welcome.blade.php
+++ resources/views/livewire/frontend/welcome.blade.php
@@ -16,7 +16,7 @@
 
                         <div>
                             <input
-                                wire:model.live.debounce.150ms="search"
+                                wire:model.live.debounce.500ms="search"
                                 id="search" name="search" type="search"
                                 class="block w-full rounded-md border-0 bg-indigo-400 bg-opacity-25 py-1.5 pl-10 pr-3 text-indigo-100 placeholder:text-indigo-200 focus:bg-white focus:text-gray-900 focus:outline-none focus:ring-0 focus:placeholder:text-gray-400 sm:text-sm sm:leading-6"
                                 placeholder="Search for Laravel packages, apps and tools">
