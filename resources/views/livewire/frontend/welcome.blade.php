ba4f1f7d7e2f173992bf807dfe2a294e5824903a
Alf Drollinger <alf@drollinger.info>
2023-09-15 10:27:51 +0200

    Hide filters in production

--- resources/views/livewire/frontend/welcome.blade.php
+++ resources/views/livewire/frontend/welcome.blade.php
@@ -27,7 +27,7 @@
                   </div>
                 </div>
 
-                @if(App::environment('production'))
+                @if(App::environment('local'))
 
                 <dl class="mt-10 grid grid-cols-1 gap-0.5 rounded-4xl text-center sm:grid-cols-2 lg:grid-cols-3">
                   <div class="flex flex-col p-4 bg-blue-900">
